3544feac3c0372ad3312c7da2002b75fe8340440
Tom Manning <tom@manningetal.com>
2020-07-03 08:52:08 -0400

    changed to use gpio_control and take command line on_off

--- heat.py
+++ heat.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
+from gpio_control import gpio_switch as switch
+# PIN_HEAT_RELAY = 25
 
-from time import sleep
-import RPi.GPIO as GPIO
-pin = 25
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)
-
-turn_on = (GPIO.input(pin) ^ 1) & 1
-print("setting relay to: {}".format(turn_on))
-GPIO.output(pin, turn_on)
-
-sleep(10)
-turn_off = (GPIO.input(pin) ^ 1) & 1
-print("setting relay to: {}".format(turn_off))
-GPIO.output(pin, turn_off)
-
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Turn coop light on/off')
+    parser.add_argument('on_off', type=int, nargs='?', choices=[0, 1], default=0, help=': 0 for oFF; 1 for oN')
+    args = parser.parse_args()
+    print("Result: {}".format(switch(25, args.on_off)))
