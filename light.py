a2de41a5132d81d5fa74b1d7619c9041aea5ff46
Tom Manning <tom@manningetal.com>
2020-07-06 06:59:15 -0400

    changed sys.exit to 0/1; use stdout for result of switch function; delete heat.py

--- light.py
+++ light.py
@@ -3,6 +3,7 @@ import sys
 from gpio_control import gpio_switch as switch
 
 if __name__ == '__main__':
+    shell_rc = 1 #default to error
     import argparse
     # print("called with {}: ".format(sys.argv[0]))
     device = None
@@ -13,27 +14,13 @@ if __name__ == '__main__':
         device = "heaters"
         pin = 25
     else:
-        sys.exit(6)
+        print("Invalid device: {}".format(sys.argv[0]))
+        sys.exit(shell_rc)
     parser = argparse.ArgumentParser(description='Turn coop {} on/off'.format(device))
     parser.add_argument('on_off', type=int, nargs='?', choices=[0, 1], default=0, help=': 0 for off; 1 for on')
     args = parser.parse_args()
     result = switch(pin, args.on_off)
-    sys.exit(result)
-'''
-old routine that toggled the light state for 10 seconds.
-
-from time import sleep
-import RPi.GPIO as GPIO
-pin = 24
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)
-
-turn_on = (GPIO.input(pin) ^ 1) & 1
-print("setting relay to: {}".format(turn_on))
-GPIO.output(pin, turn_on)
-
-sleep(10)
-turn_off = (GPIO.input(pin) ^ 1) & 1
-print("setting relay to: {}".format(turn_off))
-GPIO.output(pin, turn_off)
-'''
+    print(result)
+    if 'success' in result:
+        shell_rc = 0
+    sys.exit(shell_rc)
