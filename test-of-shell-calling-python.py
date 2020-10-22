47f9633aa0593f0092655562173070e975686d0e
Tom Manning <tom@manningetal.com>
2020-10-22 07:53:14 -0400

    test subprocess.run and launching a process without waiting for the return code.

--- test-of-shell-calling-python.py
+++ test-of-shell-calling-python.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
-#import os
 import subprocess
 import argparse
-import binascii
 
-parser = argparse.ArgumentParser(description='Test os.system')
-parser.add_argument('on_off', type=int, nargs='?', choices=[0, 1], default=0, help=': 0 for off; 1 for on')
+parser = argparse.ArgumentParser(description='Test subprocess.run (wait for process to finish) and .Popen (dont wait)')
+parser.add_argument(dest='on_off', type=int, nargs='?', choices=[0, 1], default=0, help='0 for off; 1 for on')
+parser.add_argument('-d', '--dont_wait', dest='dont_wait', action='store_true', help='run process without waiting for the process to complete')
 args = parser.parse_args()
 
-cmd = "/home/pi/coop/light.py {}".format(args.on_off)
-p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-out, err = p.communicate()
-rc = p.returncode
-if err is not None:
-    print("error code: {} occurred on calling: {}".format(err,cmd))
-# print("rc: {}".format(rc))
-if rc > 0:
-    print("script {} returned an non-zero return code: {}".format(cmd,rc))
-result = out.decode("utf-8").split('\n')
-print("output: " + result[0])
+SCRIPT = "/home/pi/coop/light.py"
+
+if args.dont_wait:
+    p = subprocess.Popen([SCRIPT, str(args.on_off)], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+    print("called {} without waiting for output.".format(SCRIPT))
+else:
+    result = subprocess.run([SCRIPT, str(args.on_off)], capture_output=True)
+    if result.returncode > 0:
+         print("{} returned an non-zero return code: {}".format(SCRIPT, result.returncode))
+    print("{} output: {}".format(SCRIPT, result.stdout.decode("utf-8").split('\n')[0]))
