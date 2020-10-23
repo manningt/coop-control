4c8806ad40972d13faca625fe605d02e3cafca38
Tom Manning <tom@manningetal.com>
2020-10-23 15:47:34 -0400

    rewrote to use gpio_control and take gpio number and activation time as optional args.

--- door-simple-with-sleep.py
+++ door-simple-with-sleep.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 
 from time import sleep
-import RPi.GPIO as GPIO
+from gpio_control import gpio_switch as switch
+import sys
+import argparse
 
-pin = 23
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)
+parser = argparse.ArgumentParser(description='Energizes relay for N seconds')
+parser.add_argument(dest='relay_pin', type=int, nargs='?', default=23, help='GPIO pin used to activate relay')
+parser.add_argument('-a', dest='activation_time', type=int, default=10, help='seconds to leave relay on')
+args = parser.parse_args()
 
-turn_on = (GPIO.input(pin) ^ 1) & 1
-print("setting relay to: {}".format(turn_on))
-GPIO.output(pin, turn_on)
+if __name__ == '__main__':
+    result = switch(args.relay_pin, 1)
+    if 'error' in result:
+        print(result)
+        sys.exit(1)
 
-sleep(10)
-turn_off = (GPIO.input(pin) ^ 1) & 1
-print("setting relay to: {}".format(turn_off))
-GPIO.output(pin, turn_off)
+    sleep(args.activation_time)
+    result = switch(args.relay_pin, 0)
+    if 'error' in result:
+        print(result)
+        sys.exit(1)
+
+    print("GPIO pin {} activated for {} seconds".format(args.relay_pin, args.activation_time))
+    sys.exit(0)
