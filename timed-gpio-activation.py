238db34a273142306a6da2ea6a060cfbac0bfb18
Tom Manning <tom@manningetal.com>
2020-10-24 12:21:19 -0400

    changed default activation time; added setting to off before activating.

--- timed-gpio-activation.py
+++ timed-gpio-activation.py
@@ -7,20 +7,26 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Energizes relay for N seconds')
 parser.add_argument(dest='relay_pin', type=int, nargs='?', default=23, help='GPIO pin used to activate relay')
-parser.add_argument('-a', dest='activation_time', type=int, default=10, help='seconds to leave relay on')
+parser.add_argument('-a', dest='activation_time', type=int, default=15, help='seconds to leave relay on')
 args = parser.parse_args()
 
 if __name__ == '__main__':
+    result = switch(args.relay_pin, 0)
+    if 'error' in result:
+        print(result)
+        sys.exit(1)
+    sleep(1)
+
     result = switch(args.relay_pin, 1)
     if 'error' in result:
         print(result)
         sys.exit(1)
-
     sleep(args.activation_time)
+
     result = switch(args.relay_pin, 0)
     if 'error' in result:
         print(result)
         sys.exit(1)
 
-    print("GPIO pin {} activated for {} seconds".format(args.relay_pin, args.activation_time))
+    print("success: GPIO pin {} activated for {} seconds".format(args.relay_pin, args.activation_time))
     sys.exit(0)
