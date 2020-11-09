f5be9af9f4fd469ddb251e72efbf121b627affe2
Tom Manning <tom@manningetal.com>
2020-11-09 05:47:12 -0500

    remove pin range check; add get_state (but commented out)

--- gpio_control.py
+++ gpio_control.py
@@ -7,9 +7,6 @@ def gpio_switch(pin, new_state):
     if new_state not in [0,1]:
         return "error: called with invalid pin state: {}".format(new_state)
 
-    if pin not in [23, 24, 25]:
-        return "error: called with invalid pin number: {}".format(pin)
-
     try:
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
@@ -22,3 +19,23 @@ def gpio_switch(pin, new_state):
     else:
         GPIO.output(pin, new_state)
         return "success"
+
+'''
+def gpio_get_state(pin):
+    try:
+        import RPi.GPIO as GPIO
+    except:
+        return "error: missing RPi GPIO"
+
+    try:
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        need to do GPIO.setup(pin, GPIO.OUT) for this to work
+    except:
+        return "error: gpio setup failed"
+
+    try:
+        return GPIO.input(pin)
+    except:
+        return "error: gpio input failed"
+'''
